f88614b88efb18063789343d7198ba426aa63d77
Mateus Santos <m.santos.azeve@gmail.com>
2021-10-04 23:01:17 -0300

    useState - React Hook (parte 1)

--- project2-reactjs-hook/src/App.js
+++ project2-reactjs-hook/src/App.js
@@ -1,22 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [reverse, setReverse] = useState(false);
+  const [counter, setCounter] = useState(0);
+  const reverseClass = reverse ? 'reverse' : '';
+
+  const handleClick = () => {
+    setReverse(!reverse);
+  };
+
+  const handleIncrement = () => {
+    setCounter(counter + 1);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className={`App-logo ${reverseClass}`} alt="logo" />
+
+        <h1>Contador: {counter}</h1>
+
+        <span>
+          <button type="button" onClick={handleClick}>
+            Reverse
+          </button>
+        </span>
+
+        <span>
+          <button type="button" onClick={handleIncrement}>
+            Increment {counter}
+          </button>
+        </span>
       </header>
     </div>
   );
